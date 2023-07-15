789aca5634be5f86d28f6e719fe8d53cf22c41c5
Nestor Nunes <109300513+netinho-RB@users.noreply.github.com>
2023-07-15 20:25:33 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-800">Bem-vindo ao meu site!</h1>
+      <p className="text-lg text-blue-600 mt-4">
+        Este é um exemplo de página usando o Tailwind CSS no Next.js.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
